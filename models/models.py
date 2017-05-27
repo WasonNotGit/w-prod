8655b71f821e206b9136b0af2d1901dffd9b1883
Wason <9617171@gmail.com>
2017-05-27 15:39:56 +0300

    Добавлено поле промомодель

--- models/models.py
+++ models/models.py
@@ -2,14 +2,19 @@
 
 from odoo import models, fields, api
 
-# class w_production_wason(models.Model):
-#     _name = 'w_production_wason.w_production_wason'
+class w_production_wason(models.Model):
+    #_name = 'res.partner'
+    _inherit = 'res.partner'
+    is_promo = fields.Boolean (string = 'Промомодель')
+    #, groups='base.group_user,base.group_system'
+    # surname = fields.Char(string = "Фамилия")
+    # name = fields.Char(string = "Имя")
+    # fathername = fields.Char(string = "Отчество")
+    #value = fields.Integer()
+    #value2 = fields.Float(compute="_value_pc", store=True)
+    #description = fields.Text()
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    #@api.depends('value')
+    #def _value_pc(self):
+    #    self.value2 = float(self.value) / 100
+ 
